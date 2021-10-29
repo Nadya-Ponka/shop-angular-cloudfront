b4e7b35fdfdb3fe6d91ff25bc566e7d0352cc432
Nadya-Ponka <ponkratova@gmail.com>
2021-10-29 21:43:48 +0200

    feat: change cart route for real EB url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,14 +11,14 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://wmijy2wwv7.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://msp12wf5x1.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://nadya-ponka-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: true,
     bff: true,
-    cart: false,
+    cart: true,
   },
 };
 
